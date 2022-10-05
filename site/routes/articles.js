b09cb0e33ad9636511eb667941e042c71453ec20
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2022-10-05 11:35:26 -0300

    Adding articles endpoint - #33

--- site/routes/articles.js
+++ site/routes/articles.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var articles = require('/home/daniel/Development/Personal_Page/site/public/content/articles/articles.json');
 var router = express.Router();
 
 /* GET about page. */
 router.get('/', function(req, res, next) {
   res.render('pages/articles/articles', { layout: 'main' });
+});
+
+router.get('/articles', function(req, res, next) {
+  res.json(articles);
 });
 
 module.exports = router;
